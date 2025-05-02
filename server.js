14712052f899e29445937b30672d2033907dc281
Eriko Barbosa <eriko.bgoncalves@gmail.com>
2025-05-02 13:56:47 -0300

    Criando algumas rotas para testar.

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+  "/": "Minha Primeira API",
+  "/contatos": "Entrei na rota de contatos",
+  "/usuarios": "Entrei na rota de usuarios",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-type": "text/plain" });
-  res.end("Welcome to my API - Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
