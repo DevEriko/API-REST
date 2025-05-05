d94bf872b6b88e8e9b35b6551d05b2bf37256d5e
Eriko Barbosa <eriko.bgoncalves@gmail.com>
2025-05-05 14:58:03 -0300

    Criando e  fazendo a conexão com o banco de dados.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import express from "express";
+import conectaNaDatabase from "./config/dbConnect.js";
+
+const conexao = await conectaNaDatabase();
+
+conexao.on("error", (erro) => {
+  console.error("erro de conexão", erro);
+});
+
+conexao.once("open", () => {
+  console.log("conectado ao banco de dados");
+});
 
 const app = express();
+app.use(express.json());
 
 const carros = [
   {
@@ -17,12 +29,42 @@ const carros = [
   },
 ];
 
+function buscarCarro(id) {
+  return carros.findIndex((carro) => {
+    return carro.id === Number(id);
+  });
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Criando a minha primeira API com Node.js");
 });
 
 app.get("/carros", (req, res) => {
   res.status(200).json(carros);
+});
+
+app.get("/carros/:id", (req, res) => {
+  const index = buscarCarro(req.params.id);
+  res.status(200).json(carros[index]);
+});
+
+app.post("/carros", (req, res) => {
+  carros.push(req.body);
+  res.status(201).send("Carro cadastrado com sucesso!");
+});
+
+app.put("/carros/:id", (req, res) => {
+  const index = buscarCarro(req.params.id);
+  carros[index].marca = req.body.marca;
+  carros[index].modelo = req.body.modelo;
+  carros[index].ano = req.body.ano;
+  res.status(200).json(carros);
+});
+
+app.delete("/carros/:id", (req, res) => {
+  const index = buscarCarro(req.params.id);
+  carros.splice(index, 1);
+  res.status(200).send("Carro removido com sucesso!");
 });
 
 export default app;
